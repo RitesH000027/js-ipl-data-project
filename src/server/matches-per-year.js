253617c614097ad3128c538df355508b08495d8f
Ritesh <rtriteshgupta2020@gmail.com>
2024-04-23 00:11:39 +0530

    commit

---
+++ src/server/matches-per-year.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const csv = require('csv-parser');
+
+const results = {};
+
+fs.createReadStream('data/matches.csv')
+  .pipe(csv())
+  .on('data', (data) => {
+    const year = data.season;
+    results[year] = (results[year] || 0) + 1;
+  })
+  .on('end', () => {
+    fs.writeFileSync('public/output/matchesPerYear.json', JSON.stringify(results, null, 4));
+    console.log('Matches per year data saved.');
+    console.log('Number of Matches Per Year:', results);
+  });
+
